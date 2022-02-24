e3ba809d6d3d6e1c36b8943bda4feda13373b664
Ryan Chang <ryanchang0501@gmail.com>
2022-02-23 20:09:39 -0500

    Initial Commit

---
+++ iTrainer/iTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTrainerApp.swift
+//  iTrainer
+//
+//  Created by Ryan Chang on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct iTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
