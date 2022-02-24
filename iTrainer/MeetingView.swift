d1add6d16c2f4ac54834c7a33355a7017184b92d
Ryan Chang <ryanchang0501@gmail.com>
2022-02-23 21:23:36 -0500

    moved progress bar to header and added place holder circle timer

--- iTrainer/MeetingView.swift
+++ iTrainer/MeetingView.swift
@@ -13,16 +13,29 @@ struct MeetingView: View {
         VStack { // this stacks the view vertically
             ProgressView(value: 5, total: 15) //this is the progress bar
             HStack { // stacks things next to each other
-                VStack{
+                VStack(alignment: .leading){ // override default alignment which is center
                     Text("Seconds Elapsed")
+                        .font(.caption) // these are modifier methods -> each modifier returns a new view
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
-                VStack{
+                Spacer() // puts a space between the vstacks
+                VStack(alignment: .trailing){
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Movement 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
         }
+        .padding()
     }
 }
 
