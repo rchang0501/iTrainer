870c706c2a8c925a0947685eec022b006dfa0182
Ryan Chang <ryanchang0501@gmail.com>
2022-02-24 20:11:36 -0500

    added list view for exercises

---
+++ iTrainer/ExerciseView.swift
@@ -0,0 +1,27 @@
+//
+//  ExerciseView.swift
+//  iTrainer
+//
+//  Created by Ryan Chang on 2022-02-24.
+//
+
+import SwiftUI
+
+struct ExerciseView: View {
+    let exercises: [DailyExercise]
+    
+    var body: some View {
+        List {
+            ForEach(exercises, id: \.title) { exercise in // ( ... ) is the initilizer
+                CardView(exercise: exercise)
+                    .listRowBackground(exercise.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ExerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseView(exercises: DailyExercise.sampleData) // initialize with class parameters
+    }
+}
