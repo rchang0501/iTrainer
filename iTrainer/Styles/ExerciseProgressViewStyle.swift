c36f95b1757f0f589bc844c7eeb6b0d85b02cee6
Ryan Chang <ryanchang0501@gmail.com>
2022-03-01 22:44:35 -0500

    added ExerciseProgressViewStyle

---
+++ iTrainer/Styles/ExerciseProgressViewStyle.swift
@@ -0,0 +1,38 @@
+//
+//  ExerciseProgressViewStyle.swift
+//  iTrainer
+//
+//  Created by Ryan Chang on 2022-03-01.
+//
+
+import SwiftUI
+
+struct ExerciseProgressViewStyle: ProgressViewStyle {
+    var theme: Theme
+    
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(theme.accentColor)
+                .frame(height: 20.0)
+            if #available(iOS 15.0, *) {
+                ProgressView(configuration)
+                    .tint(theme.mainColor)
+                    .frame(height: 12.0)
+                    .padding(.horizontal)
+            } else {
+                ProgressView(configuration)
+                    .frame(height: 12.0)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct ExerciseProgressViewStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressView(value: 0.4)
+            .progressViewStyle(ExerciseProgressViewStyle(theme: .ocean))
+            .previewLayout(.sizeThatFits)
+    }
+}
