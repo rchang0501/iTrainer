a36aa2cd46fd1de9441558c5c8fa5cce9d8bcb53
Ryan Chang <ryanchang0501@gmail.com>
2022-03-02 19:12:19 -0500

    added a @StateObject to define a source of truth to the ExerciseTimer in the TimerView

--- iTrainer/Views/TimerView.swift
+++ iTrainer/Views/TimerView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TimerView: View {
     @Binding var exercise: DailyExercise
+    @StateObject var exerciseTimer = ExerciseTimer() // @StateObject sets this instance as the source of truth 
     
     var body: some View {
         // contains the main components of the view
@@ -16,6 +17,7 @@ struct TimerView: View {
             RoundedRectangle(cornerRadius: 8.0)
                 .fill(exercise.theme.mainColor)
             VStack { // this stacks the view vertically
+                TimerHeaderView(secondsElapsed: exerciseTimer.secondsElapsed, secondsRemaining: exerciseTimer.secondsRemaining, theme: exercise.theme)
                 Circle()
                     .strokeBorder(lineWidth: 24)
                 HStack {
