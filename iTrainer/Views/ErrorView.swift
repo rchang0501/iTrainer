1e45b3d92ae371b4603d285eabf7ca09d86aa5ce
Ryan Chang <ryanchang0501@gmail.com>
2022-03-08 20:09:50 -0500

    added an error view

---
+++ iTrainer/Views/ErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorView.swift
+//  iTrainer
+//
+//  Created by Ryan Chang on 2022-03-08.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView()
+    }
+}
